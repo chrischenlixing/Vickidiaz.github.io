aa6517294bdba1a7e2d998d9970d61e1cfc0a35c
USER1 <user1@Vickis-MacBook-Air.local>
2023-09-18 16:56:49 -0700

    centered nav bar, added content

--- script.js
+++ script.js
@@ -1,20 +1,21 @@
-
-function chargebattery() {
-  var a;
-  a = document.getElementById("battery");
-  a.innerHTML = "&#xf244;";
-  setTimeout(function () {
-      a.innerHTML = "&#xf243;";
-    }, 1000);
-  setTimeout(function () {
-      a.innerHTML = "&#xf242;";
-    }, 2000);
-  setTimeout(function () {
-      a.innerHTML = "&#xf241;";
-    }, 3000);
-  setTimeout(function () {
-      a.innerHTML = "&#xf240;";
-    }, 4000);
-}
-chargebattery();
-setInterval(chargebattery, 5000);
+// no loger part of design 
+// Animate Icons effect help from w3schools 
+// function chargebattery() {
+//   var a;
+//   a = document.getElementById("battery");
+//   a.innerHTML = "&#xf244;";
+//   setTimeout(function () {
+//       a.innerHTML = "&#xf243;";
+//     }, 1000);
+//   setTimeout(function () {
+//       a.innerHTML = "&#xf242;";
+//     }, 2000);
+//   setTimeout(function () {
+//       a.innerHTML = "&#xf241;";
+//     }, 3000);
+//   setTimeout(function () {
+//       a.innerHTML = "&#xf240;";
+//     }, 4000);
+// }
+// chargebattery();
+// setInterval(chargebattery, 5000);
