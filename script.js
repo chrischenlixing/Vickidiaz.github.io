587c66b407327b1679b4a204bf14cbe9f29fe0fa
USER1 <user1@Vickis-MacBook-Air.local>
2023-09-18 18:46:55 -0700

    updated nav bar to buttons, more content added

--- script.js
+++ script.js
@@ -19,3 +19,21 @@
 // }
 // chargebattery();
 // setInterval(chargebattery, 5000);
+
+// script.js
+// script.js
+document.addEventListener("DOMContentLoaded", function () {
+    var playButton = document.getElementById("playButton");
+    var music = document.getElementById("music");
+  
+    playButton.addEventListener("click", function () {
+      if (music.paused) {
+        music.play();
+        playButton.textContent = "Pause Music";
+      } else {
+        music.pause();
+        playButton.textContent = "Play Music";
+      }
+    });
+  });
+  
