3127f55198acc7a556dd9c4b311a71607451a972
USER1 <user1@Vickis-MacBook-Air.local>
2023-09-19 12:21:28 -0700

    remove unused code

--- script.js
+++ script.js
@@ -1,26 +1,3 @@
-// no loger part of design 
-// Animate Icons effect help from w3schools 
-// function chargebattery() {
-//   var a;
-//   a = document.getElementById("battery");
-//   a.innerHTML = "&#xf244;";
-//   setTimeout(function () {
-//       a.innerHTML = "&#xf243;";
-//     }, 1000);
-//   setTimeout(function () {
-//       a.innerHTML = "&#xf242;";
-//     }, 2000);
-//   setTimeout(function () {
-//       a.innerHTML = "&#xf241;";
-//     }, 3000);
-//   setTimeout(function () {
-//       a.innerHTML = "&#xf240;";
-//     }, 4000);
-// }
-// chargebattery();
-// setInterval(chargebattery, 5000);
-
-// script.js
 // script.js
 document.addEventListener("DOMContentLoaded", function () {
     var playButton = document.getElementById("playButton");
